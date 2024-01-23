4310f0fff0d142c2842eff509bb4a0b9fc74f677
minari314159 <127341132+minari314159@users.noreply.github.com>
2024-01-23 15:19:25 -0500

    Implement model training

---
+++ create_model.py
@@ -0,0 +1,72 @@
+import pandas as pd
+from sklearn.metrics import mean_squared_error, accuracy_score
+from sklearn.model_selection import TimeSeriesSplit
+import xgboost as xgb
+
+def create_model(data, n_estimators):
+    tss = TimeSeriesSplit(n_splits=5, test_size=24*365*1, gap=24)
+    data = data.sort_index()
+
+    fold = 0
+    preds = []
+    scores = []
+
+    def create_features(df):
+        df = df.copy()
+        df['hour'] = df.index.hour
+        df['month'] = df.index.month
+        df['year'] = df.index.year
+
+        return df
+        
+    data = create_features(data)
+
+    def add_lags(df):
+        target_map = df['Energy Use (MW)'].to_dict()
+        df['1 year'] = (df.index - pd.Timedelta('364 days')).map(target_map)
+        df['2 years'] = (df.index - pd.Timedelta('728 days')).map(target_map)
+        df['3 years'] = (df.index - pd.Timedelta('1092 days')).map(target_map)    
+
+        return df
+        
+    data = add_lags(data)
+        
+    for train_index, val_index in tss.split(data):
+        train = data.iloc[train_index]
+        test = data.iloc[val_index]
+        
+        train = create_features(train)
+        test = create_features(test)
+
+        FEATURES = ['hour', 'month', 'year', '1 year', '2 years', '3 years']
+        TARGET = 'Energy Use (MW)'
+
+        x_train = train[FEATURES]
+        y_train = train[TARGET]
+
+        x_test = test[FEATURES]
+        y_test = test[TARGET]
+
+        reg = xgb.XGBRegressor(
+            n_estimators=n_estimators, 
+            early_stopping_rounds=50, 
+            learning_rate=0.01,
+            objective='reg:squarederror',
+            max_depth=1,
+            base_score=0.5)
+        reg = reg.fit(x_train, y_train,
+            eval_set=[(x_train, y_train), (x_test, y_test)],
+            verbose=100)
+        
+        y_pred = reg.predict(x_test)
+        preds.append(y_pred)
+        scores.append(scores)
+        fold += 1
+
+    test['prediction'] = reg.predict(x_test)
+    data = data.merge(test[['prediction']],
+                      how='left',
+                      left_index=True,
+                      right_index=True)
+    
+    return reg, data, preds, scores
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,20 @@
-import streamlit as st
-import pandas as pd
-import plotly.express as px
 
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go 
+import pandas as pd 
+import numpy as np
+from sklearn.metrics import mean_absolute_percentage_error
+from DataSet.load_data import load_PJME, load_PJMW
+from create_model import create_model
+
+
+#page outline
 header = st.container()
 dataset = st.container()
 features = st.container()
 modelTraining = st.container()
-predictions = st.container()
+future = st.container()
 sidebar = st.container()
 
 with open('styles.css') as f:
@@ -21,8 +29,9 @@ with sidebar:
     st.sidebar.divider()
     west_east = st.sidebar.selectbox(
         'Choose west or east dataset', options=['PJM West Region', 'PJM East Region'], index=1)
-    max_depth = st.sidebar.slider('Choose models max depth', min_value=10, max_value=100, value=50)
-    n_estimators = st.sidebar.selectbox('How far in the future to predict', options=[0.5,1,2], index=1)
+    n_estimators = st.sidebar.slider(
+        'Choose n estimators for the model', min_value=500, max_value=1000, value=500)
+    future_predictions = st.sidebar.selectbox('How far in the future to predict', options=[0.5,1,2], index=1)
 
     st.sidebar.markdown('''
     ---
@@ -38,41 +47,158 @@ with header:
 with dataset:
     st.header('PJM Hourly Energy Consumption Data')
      
-    df_East = pd.read_csv('DataSet/Regions/PJME_hourly.csv').set_index('Datetime').query('PJME_MW > 19_000').rename(
-        {'PJME_MW': 'Energy Use E (MW)'}, axis=1)
-    df_East.index = pd.to_datetime(df_East.index)
-    fig = px.line(df_East, x=df_East.index,
-                  y=df_East['Energy Use E (MW)'])
-    fig.update_layout(
-        showlegend= False,
-        width=800,
-        height=500,
-        xaxis=dict(
-            showline=True,
-            linecolor='#787a79',
-            linewidth=1
-        ),
-        yaxis=dict(
-            showline=True,
-            linecolor='#787a79',
-            linewidth=1
-        )
-    )
-    fig.update_xaxes(minor=dict(ticks="outside", showgrid=True),
-                     rangeslider_visible=True)
-    fig.update_traces(line_color="#f7768e")
-    st.write(fig)
+    col1, col2 = st.columns((6,4))
+    df_East = load_PJME()
+    df_West = load_PJMW()
 
+    if west_east == 'PJM East Region':
+        data = df_East
+    else:
+        data = df_West
+
+    with col1:
+        fig = px.line(data, x=data.index,
+                    y=data['Energy Use (MW)'])
+
+        fig.update_layout(
+            showlegend= False,
+            width=800,
+            height=500,
+            title='Energy Consumption 2002-2018',
+            xaxis=dict(
+                showline=True,
+                linecolor='#787a79',
+                linewidth=1
+            ),
+            yaxis=dict(
+                showline=True,
+                linecolor='#787a79',
+                linewidth=1
+            )
+        )
+        fig.update_xaxes(minor=dict(ticks="outside", showgrid=True),
+                        rangeslider_visible=True)
+        fig.update_traces(line_color="#f7768e")
+        st.plotly_chart(fig, use_container_width=True)
+
+    with col2:
+        fig = px.histogram(
+            data, x=data['Energy Use (MW)'], nbins=500, 
+            width=400,
+            height=350,
+            title='Frequency of Energy Consumption',
+            color_discrete_sequence=['#f7768e'])
+        st.plotly_chart(fig, use_container_width=True)
+        
 
 with features:
     st.header('Features')
+    col1,col2 = st.columns((3,7))
+    with col1:
+        st.markdown('* **Hour Feature**')
+        st.markdown('* **Month Feature**')
+        st.markdown('* **Year Feature**')
+    
+    with col2:
+        reg = create_model(data, n_estimators)[0]
 
-    st.markdown('* **First Feature**: Why this feature, how was it calculated')
-    st.markdown('* **Second Feature**: Why this feature, how was it calculated')
-    st.markdown('* **Third Feature**: Why this feature, how was it calculated')
+        fi = pd.DataFrame(data=reg.feature_importances_,
+                          index=reg.feature_names_in_,
+                          columns=['Importance'])
+        
+        fig = px.bar(fi.iloc[0:3], labels={'index':'Feature', 'value':'Importance'})
+        fig.update_layout(
+            showlegend=False,
+            width=700,
+            height=300,
+            title='Feature Importance',
+            xaxis=dict(
+                showline=True,
+                linecolor='#787a79',
+                linewidth=1
+            ),
+            yaxis=dict(
+                showline=True,
+                linecolor='#787a79',
+                linewidth=1
+            )
+        )
+        fig.update_traces(marker_color='#f7768e')
+        st.plotly_chart(fig, use_container_width=True)
+        
+    st.markdown('**Lag Features** were calculated for 1,2 and 3 years. Indicating the validity of predicted data from 1-3 years in advance')
+    fig = px.bar(fi.iloc[3:], labels={'index':'Lag Features', 'value':'Importance'})
+    fig.update_layout(
+            showlegend=False,
+            width=700,
+            height=300,
+            title='Lag Importance',
+            xaxis=dict(
+                showline=True,
+                linecolor='#787a79',
+                linewidth=1
+            ),
+            yaxis=dict(
+                showline=True,
+                linecolor='#787a79',
+                linewidth=1
+            )
+        )
+    fig.update_traces(marker_color='#f7768e')
+    st.plotly_chart(fig, use_container_width=True)
 
 with modelTraining:
     st.header('Model Training')
+    
+    df_preds = pd.DataFrame(create_model(data, n_estimators)[1])
+    df_preds['prediction'] = df_preds['prediction'].fillna(0)
+    df_preds = df_preds.query('prediction > 19_000')
+  
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df_preds.index,
+                  y=df_preds['prediction'], 
+                  name='Predicted Set', 
+                  mode='lines',
+                  line={'color': '#f7768e'}))
 
-with predictions:
-    st.header('Predictions')
+    fig.add_trace(go.Scatter(x=df_preds.index,
+                    y=df_preds['Energy Use (MW)'],
+                    name='Test Set',
+                    mode='lines',
+                    line={'color': '#8576f7'},
+                    opacity=0.5))
+    fig.update_layout(
+            showlegend= True,
+            width=800,
+            height=500,
+            title='Energy Consumption 20-2018',
+            xaxis=dict(
+                showline=True,
+                linecolor='#787a79',
+                linewidth=1
+            ),
+            yaxis=dict(
+                showline=True,
+                linecolor='#787a79',
+                linewidth=1
+            )
+        )
+    fig.update_xaxes(minor=dict(ticks="outside", showgrid=True),
+                        rangeslider_visible=True)
+    
+    st.plotly_chart(fig, use_container_width=True)
+
+    score = mean_absolute_percentage_error(
+        df_preds['Energy Use (MW)'], 
+        df_preds['prediction'])
+    st.write(f'MAPE Score:` {score:0.2f}`')
+
+    st.markdown('### Improvements')
+    st.markdown('**The following are suggested improvements to lower the RMSE score:**')
+    st.markdown("* Add features including holidays, moderate to extreme weather ")
+    st.markdown("* Both of these will improve the predictions for those 'outlier' days during the month of varying energy consumption that don't follow the seasonal pattern")
+    st.markdown("* Reduce over fitting inherent to XGBoost models")
+
+with future:
+    st.header('Forcasting')
+    
